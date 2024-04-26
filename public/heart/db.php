e4f15616ce1edda243050ca6a126c307a967dab7
popovalnik <92301379+popovalnik@users.noreply.github.com>
2024-04-26 20:56:55 +0300

    Update db.php

--- public/heart/db.php
+++ public/heart/db.php
@@ -1,4 +1,4 @@
 <?
-$mysqli = mysqli_connect("localhost", "047189102_ex", "6Rge1ta67m", "popovalnik_ex") or die('Ошибка соединения с базой данных!');
+$mysqli = mysqli_connect("localhost", "login", "password", "popovalnik_ex") or die('Ошибка соединения с базой данных!');
 mysqli_query($mysqli, "SET NAMES utf8");
 ?>
